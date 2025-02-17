a7554ef3463683a1de2bb97350abd74b8c5bc5b5
Peter Dulacka <dulacka@gmail.com>
2025-02-17 06:16:05 +0000

    Update Nette extension config definition to use config definition schema
    remp/crm#3417

--- src/DI/StripeModuleExtension.php
+++ src/DI/StripeModuleExtension.php
@@ -8,13 +8,8 @@ use Nette\DI\CompilerExtension;
 
 final class StripeModuleExtension extends CompilerExtension implements TranslationProviderInterface
 {
-    private $defaults = [];
-
     public function loadConfiguration()
     {
-        // set default values if user didn't define them
-        $this->config = $this->validateConfig($this->defaults);
-
         // load services from config and register them to Nette\DI Container
         $this->compiler->loadDefinitionsFromConfig(
             $this->loadFromFile(__DIR__.'/../config/config.neon')['services']
